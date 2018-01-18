8fb689d3e3905183c2d11425c265682cbee6d551
Lezh1k <lezh1k.vohrer@gmail.com>
2018-01-18 17:45:30 +0600

    Optimized prepare script

--- prepare.sh
+++ prepare.sh
@@ -2,8 +2,6 @@
 SRC=$1
 RES=log1sorted
 TMP=log1
-cut -c 23- $SRC > $TMP
-sort -k1.2 $TMP > $RES 
+cut -c 23- $SRC | sort -k1.2 > $RES
 perl -p -i -e "s/,/./g" $RES 
 perl -p -i -e "s/\. /, /g" $RES 
-rm $TMP
