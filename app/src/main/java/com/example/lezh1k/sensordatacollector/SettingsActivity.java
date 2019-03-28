b7036713955f0253c134804132e54f9e5536eff7
Vin Busquet <computationalcore@gmail.com>
2019-03-27 23:22:42 -0300

    feat: Add settings layout and activity

---
+++ app/src/main/java/com/example/lezh1k/sensordatacollector/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.lezh1k.sensordatacollector;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/example/lezh1k/sensordatacollector/SettingsFragment.java
@@ -0,0 +1,11 @@
+package com.example.lezh1k.sensordatacollector;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+}
