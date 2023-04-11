8ae7a82b9802cda16f4e198928db7c1d3a11a912
Oleg Katkov <lezh1k.vohrer@gmail.com>
2023-04-12 03:39:06 +0600

    Removed .cache again

--- data_generator/mlm_data_generator/src/main.cpp
+++ data_generator/mlm_data_generator/src/main.cpp
@@ -3,11 +3,18 @@
 #include "sd_generator.h"
 #include "sensor_data.h"
 #include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
   UNUSED(argc);
   UNUSED(argv);
 
-  movement_interval_t intervals[] = {};
-  gps_coordinate_t start;
+  std::vector<movement_interval_t> intervals = {
+    {0.0, 3.0, 5.0},
+  };
+
+  for (auto interval : intervals) {
+    std::cout << interval.azimuth << " " << interval.duration << " " << interval.acceleration << "\n";
+  }
+
 }
